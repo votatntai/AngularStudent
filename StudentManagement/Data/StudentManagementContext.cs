323bc21dced2ebe63219ae053dcebc113261778d
Janglee <votantai4899@gmail.com>
2020-10-18 22:33:30 +0700

    Add project files.

---
+++ StudentManagement/Data/StudentManagementContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentManagement.Data
+{
+    public class StudentManagementContext : DbContext
+    {
+        public StudentManagementContext(DbContextOptions<StudentManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentManagement.Entities.Student> Student { get; set; }
+    }
+}
---
+++ StudentManagement/Entities/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentManagement.Entities
+{
+    [Table("Students")]
+    public class Student
+    {
+        [Key]
+        public Guid StudentId { get; set; }
+
+        [MaxLength(256)]
+        public string StudentName { get; set; }
+
+        [MaxLength(256)]
+        public string Email { get; set; }
+
+        [MaxLength(256)]
+        public string Address { get; set; }
+    }
+}
