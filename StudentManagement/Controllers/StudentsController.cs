d5c0f7fb8c810f7d6bd295b6f642945f2333496a
Janglee <votantai4899@gmail.com>
2020-10-19 13:58:59 +0700

    Allow more origins

--- StudentManagement/Controllers/StudentsController.cs
+++ StudentManagement/Controllers/StudentsController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StudentManagement.Data;
 using StudentManagement.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace StudentManagement.Controllers
 {
